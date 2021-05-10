7e429428a5e25bbb95b1c7362a53a6ca886bfc75
张彦通 <1678532032@qq.com>
2021-05-10 21:37:54 +0800

    删除整理项目中未使用的布局文件

--- app/src/main/java/cn/com/lttc/loginui/utils/GridImageAdapter.java
+++ app/src/main/java/cn/com/lttc/loginui/utils/GridImageAdapter.java
@@ -9,9 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
 import cn.com.lttc.loginui.R;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
